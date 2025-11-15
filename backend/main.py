bc4b82235f0937b3775184f227e82568a88d5fb3
danruk <149634927+danruk@users.noreply.github.com>
2025-11-15 00:35:48 -0800

    fix a bug

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend.routers.search import router as search_router
 from backend.routers.flags_router import router as flags_router
 from backend.routers.penalties_router import router as penalties_router
+from backend.routers.watchlist_router import router as watchlist_router
 
 app = FastAPI(title="COSC310 API (dev)")
 
